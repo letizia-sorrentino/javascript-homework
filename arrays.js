17f14eb286b19d3dddcf8d810a410f3be665b8e4
Letizia <116899981+letizia-sorrentino@users.noreply.github.com>
2023-04-04 07:26:06 +0100

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,90 @@
+// 1. Create an array with 10 items
+rainbowArray = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'white', 'black', 'brown'];
+
+// 2.  Update one of the items 
+rainbowArray[0] = 'pink';
+console.log(rainbowArray);
+
+// 3. Delete half of the array
+rainbowArray.splice(5, 5);
+console.log(rainbowArray);
+
+// 4. Write out each of the array methods so you become familiar with the syntax (use the array you made earlier)   
+
+//Pop
+rainbowArray.pop();
+console.log(rainbowArray);
+
+//Push
+rainbowArray.push('purple', 'turquoise', 'grey');
+console.log(rainbowArray);
+
+//Shift
+rainbowArray.shift();
+console.log(rainbowArray);
+
+//Unshift
+rainbowArray.unshift('neonred');
+console.log(rainbowArray);
+
+//Concatenating
+const otherColours = ['beige', 'salmon'];
+
+const newRainbow = rainbowArray.concat(otherColours);
+console.log(newRainbow);
+
+//Splicing
+rainbowArray.splice(5, 1);
+console.log(rainbowArray);
+
+//Slicing
+const newRainbow2 = rainbowArray.slice(0, 2);
+console.log(newRainbow2);
+
+//Reverse
+const reversedArray = rainbowArray.reverse();
+console.log(reversedArray);
+
+//Join
+const joinedArray = rainbowArray.join(' - ');
+console.log(joinedArray);
+
+//forEach
+rainbowArray.forEach(colour => console.log(colour));
+
+//Includes
+console.log(rainbowArray.includes('yellow'));
+
+//indexOf
+console.log(rainbowArray.indexOf('grey'));
+
+//Every - TO BE DOUBLE-CHECKED
+const checkRainbow = (colour) => {colour.lenght > 2 };
+console.log(rainbowArray.every(checkRainbow));
+
+//Some - TO BE DOUBLE-CHECKED
+const checkColours = (colour) => {colour === 'orange'};
+console.log(rainbowArray.some(checkColours));
+
+//Filter
+const colourFilter = rainbowArray.filter(colour => colour.length < 5);
+console.log(colourFilter);
+
+//Find
+const found = rainbowArray.find(colour => colour === 'grey');
+console.log(found);
+
+//findIndex
+const colourIndex = (colour) => colour === 'neonred';
+console.log(rainbowArray.findIndex(colourIndex));
+
+//Sort
+rainbowArray.sort();
+console.log(rainbowArray);
+
+//Map
+const mapColours = rainbowArray.map(function (colour, index) {
+    return colour + index;
+});
+console.log(mapColours);
+
---
+++ functions.js
@@ -0,0 +1,111 @@
+// 1.	Create a function, inside the function add a second function, and again add a third function into the second function.
+// 2.	Create 3 variables.
+// 3.	Define 3 the variables in the correct place so that:
+//  a.	The first variable is accessible inside every function.
+//  b.	The second variable is accessible inside the second and third function only.
+//  c.	The third variable is accessible inside the third function only.
+// 4.	Define a 4th variable inside the 3rd function. This variable should be accessible by every function.
+// 5.	Create a scope inside the 3rd function, place a variable inside it that is only accessible inside that scope.
+
+function first() {
+    var one = 'one';
+    console.log(one);
+
+    function second() {
+        var two = 'two';
+        console.log(one, two);
+
+        function third() {
+            let three = 'three';
+            var four = 'four';
+            console.log(one, two, three, four);
+
+            {
+                let five = 'five';
+                console.log(five);
+            };
+
+        };
+        third();
+    };
+    second();
+};
+first();
+
+// 6. Create an object which contains a function which console.logs this.something, where something is any name you like.
+
+const person = {
+    firstName: 'Letizia',
+    sayHello: function () {
+        console.log(`Hello ${this.firstName}!`)
+    }
+};
+
+person.sayHello();
+
+// 7. Create an object that contains information about the make/model of a car
+const car = {
+    make: 'BMW',
+    model: 'Mini',
+}
+
+// 8. Create a function outside the object which returns the make/model of the car
+
+car.type = function () {
+    return this.make + " " + this.model;
+};
+
+// 9. Insert a reference to the function into the object created
+Object.defineProperty(car, 'type', function () { return this.make + " " + this.model });
+
+// 10. Invoke the method on the object (you should see the make/model)
+console.log(car.type());
+
+// 11. Use call to execute the function using a different context
+console.log(car.type.call(car));
+
+// 12. Use bind to create an instance of the function that is bound to a different object
+const car2 = {
+    make: 'FIAT',
+    model: '500',
+};
+
+let type = car.type.bind(car2);
+
+console.log(car.type.call(car2));
+
+// 13. Use a try/catch block to ensure you understand the syntax
+try {
+    console.log(ciao)
+}
+catch (error) {
+    console.log('That did not work, but it did not crash');
+} finally {
+    console.log('I always run')
+};
+
+// 14. Create a timer that runs once
+function handler() {
+    console.log('Handler ran as a result of a timer.');
+}
+
+const myTimeout = setTimeout(handler, 1500);
+console.log('Handler ran only once')
+
+function myStopFunction() {
+    clearTimeout(myTimeout);
+}
+// 15. Create a timer that runs many times
+
+const myTimer = setTimeout(handler, 1 * 200);
+console.log('Handler ran many times.');
+
+// 16. Clear one of the timers
+setTimeout(function () {
+    clearInterval(myTimer), 1500
+})
+
+// 17. Use a ternary
+const weather = 'rain';
+const forecast = 'rain' ? 'it is going to be rainy' : 'it is going to be sunny';
+console.log(forecast);
